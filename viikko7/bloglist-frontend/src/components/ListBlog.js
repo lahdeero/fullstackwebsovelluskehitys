f188b9492490413c8dc09e49cac0ffffac64ec09
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-05-15 13:09:21 +0300

    something in listblog

--- viikko7/bloglist-frontend/src/components/ListBlog.js
+++ viikko7/bloglist-frontend/src/components/ListBlog.js
@@ -1,37 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-const ListBlog = ({ blog, like, remove, creator }) => {
-  const [expanded, setExpanded] = useState(false)
-
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
-
-  const details = () => (
-    <div className='details'>
-      <a href={blog.url}>{blog.url}</a>
-      <div>{blog.likes} likes
-        <button onClick={() => like(blog)}>like</button>
-      </div>
-      <div>added by {blog.user.name}</div>
-      {creator && (<button onClick={() => remove(blog)}>remove </button>)}
-    </div>
-  )
-
-  // return (
-  //   <div style={blogStyle}>
-  //     <div onClick={() => setExpanded(!expanded)} className='name'>
-  //       {blog.title} {blog.author}
-  //     </div>
-  //     {expanded && details()}
-  //   </div>
-  // )
+const ListBlog = ({ blog }) => {
   return (
     <div>
       <Link to={`/blogs/${blog.id}`}>{blog.title} {blog.author}</Link>
